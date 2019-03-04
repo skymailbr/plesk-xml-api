e1b5db7bb3ef98abbe9bb38b960ecf1c7886815b
Lucas Stevanelli Marin <lucasmarin@users.noreply.github.com>
2019-03-04 00:35:45 -0300

    Bugfix: Always returning one result on get
    Changing logic of get method on PHPHanlder Operator to get all PHPHandler available on plesk

--- src/PleskX/Api/Operator/PHPHandler.php
+++ src/PleskX/Api/Operator/PHPHandler.php
@@ -2,6 +2,8 @@
 // Copyright 1999-2015. Parallels IP Holdings GmbH.
 
 namespace PleskX\Api\Operator;
+
+use PleskX\Api\Client;
 use PleskX\Api\Struct\PHPHandler as Struct;
 
 class PHPHandler extends \PleskX\Api\Operator
@@ -10,16 +12,33 @@ class PHPHandler extends \PleskX\Api\Operator
     /**
      * @param string $field
      * @param integer|string $value
-     * @return Struct\Info
+     *
+     * @return array|Struct\Info
      */
     public function get($field = null, $value = null)
     {
-        $ips = [];
         $packet = $this->_client->getPacket();
+
         $filter = $packet->addChild('php-handler')->addChild('get')->addChild('filter');
-        if ($field) $filter->addChild($field, $value);
-        $response = $this->_client->request($packet);
-        return new Struct\Info($response);
+        if ($field) {
+            $filter->addChild($field, $value);
+        }
+
+        $response = $this->_client->request($packet, \PleskX\Api\Client::RESPONSE_FULL)->{'php-handler'}->get->result;
+
+        $ret = null;
+        if ($field == 'id' && isset($response->id)) {
+            $ret = new Struct\Info($response);
+        } else {
+            $ret = [];
+            foreach ($response as $f) {
+                if (isset($f->id)) {
+                    $ret[] = new Struct\Info($f);
+                }
+            }
+        }
+
+        return $ret;
     }
 
 }
